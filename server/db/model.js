8b86ff00bea2d493483c7dee85d69b16344237ae
Brandon Foster <Brandon.Foster81@myhunter.cuny.edu>
2020-01-11 06:53:52 -0500

    modified model.js
    Added PostSchema
    Added descriptions for each property

--- server/db/model.js
+++ server/db/model.js
@@ -1,20 +1,57 @@
 import mongoose from "mongoose";
 
 // experimental , not to be used yet
+
 export const UserSchema = new mongoose.Schema({
+  /** The user's first name */
   firstName: {
     type: String
   },
+  /** The user's last name */
   lastName: {
     type: String
   },
+  /** The user's email address */
   email: {
     type: String
   },
+  /** Array of the user's posted messages */
   posts: {
     type: Array
   },
+  /** User is treated as an administrator if true */
   isAdmin: {
     type: Boolean
+  }
+});
+
+export const PostSchema = new mongoose.Schema({
+  /** The user that created the post */
+  poster: {
+    type: Schema.Types.ObjectId
+  },
+  /** The text string of the post message */
+  text: {
+    type: String
+  },
+  /** Array of replied messages */
+  replies: {
+    type: Array
+  },
+  /** Only visible to administrators if true */
+  private: {
+    type: Boolean
+  },
+  /** Poster's name is replaced with 'anonymous' if true */
+  anonymity: {
+    type: Boolean
+  },
+  /** Array of users that like the comment or found it helpful */
+  upvotes: {
+    type: Array
+  },
+  /** Array of users that dislike the comment or found it unhelpful */
+  downvotes: {
+    type: Array
   }
 });
