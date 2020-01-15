1a80e79fab15b9acf92ca1834e21ebb81e7b6fd8
Josh Winton <joshcwinton@gmail.com>
2020-01-15 17:29:01 -0500

    google auth again

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+import { GoogleLogin } from 'react-google-login';
 
 export default class Login extends Component {
   render() {
-    return <div>Login Page</div>;
+    <React.Fragment>
+      <GoogleLogin
+        clientId="1041011900309-m7q81mcbv6j6cnbaba03v9n9fs0d4iga.apps.googleusercontent.com"
+        render={renderProps => (
+          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+        )}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />,
+      document.getElementById('googleButton')
+    </React.Fragment>
   }
 }
