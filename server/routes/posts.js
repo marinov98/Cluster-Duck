9edfd2eb0b02d60e91dd8999544d9b65dbeaebbf
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-01-15 17:59:53 -0500

    initial post routes setup

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,1 +1,30 @@
 // posts routes
+import express from "express";
+import { Post } from "./../db/models";
+const router = express.Router();
+
+/**
+ * CreatePost endpoint
+ * @route POST /p
+ * @desc Create a post
+ * @access Public
+ */
+router.post("/", async (req, res, next) => {});
+
+/**
+ * FindAllPosts endpoint
+ * @route GET /p
+ * @desc Find all posts
+ * @access Public
+ */
+router.get("/", async (req, res, next) => {});
+
+/**
+ * DeletePost endpoint
+ * @route DELETE /p/:postId
+ * @desc Delete a post
+ * @access Public
+ */
+router.delete("/:postId", async (req, res, next) => {});
+
+export default router;
