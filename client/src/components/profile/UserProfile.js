28cedb1c18cc26c03290aeb3566c40b7c6317910
Stephen K <stephenjk@optonline.net>
2020-01-15 18:05:26 -0500

    simple prop implementation

--- client/src/components/profile/UserProfile.js
+++ client/src/components/profile/UserProfile.js
@@ -1,9 +1,23 @@
-import React, Component from "react";
+import React, { Component } from "react";
 
 export default class UserProfile extends Component {
-	render() {
+	constructor(props){
+		super(props);
+		this.state={
+				profile_info:this.props.profile_info,
+				current_classes:this.props.current_classes,
+				past_classes: this.props.past_classes
+			};
+	}
 
+	render() {
+			console.log(this.state.profile_info)
+			console.log(this.state.current_classes)
+			console.log(this.state.past_classes)
+		return (
+
+			<div> "Hello World!" </div> 
+		);
 	}
 }
 
-export default UserProfile;
