a88a852cbd084ea0cbe64a281f431ce68dc38807
joshcwinton <joshcwinton@gmail.com>
2020-01-15 23:49:23 -0500

    - Google login connects to API properly - set port back to 5000

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import { GoogleLogin } from 'react-google-login';
 
+const responseGoogleGood = (response) => {
+  // Send web token to backend
+  console.log("Successful login!", response);
+}
+
+const responseGoogleBad = (response) => {
+  console.log("Something is wrong with login...", response);
+}
+
 export default class Login extends Component {
   render() {
-    <React.Fragment>
-      <GoogleLogin
-        clientId="1041011900309-m7q81mcbv6j6cnbaba03v9n9fs0d4iga.apps.googleusercontent.com"
-        render={renderProps => (
-          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
-        )}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />,
-      document.getElementById('googleButton')
-    </React.Fragment>
+    return(
+      <div>
+        <GoogleLogin
+          clientId="1041011900309-oaq4n0svcmrocdf4q3q9hdgpudlfllfs.apps.googleusercontent.com"
+          render={renderProps => (
+            <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+          )}
+          buttonText="Login"
+          onSuccess={responseGoogleGood}
+          onFailure={responseGoogleBad}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    );
   }
 }
