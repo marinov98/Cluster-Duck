aa02d4000c9895fe70bdd2156f09b34f13c3bc8b
Stephen K <stephenjk@optonline.net>
2020-01-16 20:39:27 -0500

    add containers for profile info and profile classes

---
+++ client/src/components/profile/containers/ProfileClasses.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProfileClasses(){
+	console.log("Pclasses");
+	return (
+		<div/>
+	);
+}
+
+export default ProfileClasses;
---
+++ client/src/components/profile/containers/ProfileInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfileInfo(){
+
+	console.log("pInfo");
+	return (
+		<div />
+	);
+}
+
+export default ProfileInfo;
