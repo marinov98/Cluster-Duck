510594f8b956b6d9e36ee1d66d8e741ee9705065
Belinda Liu <belindaliu@Belindas-MacBook-Pro.local>
2020-01-16 19:12:54 -0500

    added simple list of items to be added to homepage (dummy data)

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class Home extends React.Component {
     render(){
-        return(<div>"HomePage"</div>);
+        return(
+        <React.Fragment>
+          <div>"ADD NAV BAR"</div>
+          <div>"ADD PROFILE HERE"</div>
+          <div>"ADD NOTIFICATIONS HERE"</div>
+          <div>"ADD CLASSES/CATEGORIES HERE"</div>
+          <div>"ADD FEED"</div>
+        </React.Fragment>    
+
+        );
     }
 }
 
