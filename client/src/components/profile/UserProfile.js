b0704603d2a8e51c1c4c0cc6fffdadb8e3faa82d
Stephen K <stephenjk@optonline.net>
2020-01-16 20:55:00 -0500

    add documentation for UserProfile component

--- client/src/components/profile/UserProfile.js
+++ client/src/components/profile/UserProfile.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import ProfileInfo from "./containers/ProfileInfo.js";
+import ProfileClasses from "./containers/ProfileClasses.js";
+import RecentPosts from "./RecentPosts.js";
 
 export default class UserProfile extends Component {
 	constructor(props){
@@ -15,9 +18,41 @@ export default class UserProfile extends Component {
 			console.log(this.state.current_classes)
 			console.log(this.state.past_classes)
 		return (
-
+			<div>
+			{/* Navbar */}
 			<div> "Hello World!" </div> 
+				<img />
+			{/* Profile image 
+				input: image object retrieved from remote file storage into image tag
+				output: image */}
+				<div>
+
+			{/* Profile Info 
+				input: object
+				output: column + row with identifiers points */}
+					<ProfileInfo />
+				</div>
+
+				<div>
+			{/* Current Classes 
+				input: object
+				output: column + row with bulleted points */}
+					<ProfileClasses classType={"Current"}/>
+				</div>
+
+				<div>
+			{/* Past Classes 
+				input: object
+				output: column + row with bulleted points */}
+					<ProfileClasses classType={"Past"}/>
+				</div>
+			{/* Recent Posts
+				input: object
+				output: list of most recent posts */}	
+				<RecentPosts posts={["lorem,ipsum"]}/>
+			</div>
 		);
 	}
 }
 
+// create containers
