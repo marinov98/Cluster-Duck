ba1df21811e6607c6e06490e37388d40fc794125
Stephen K <stephenjk@optonline.net>
2020-01-17 17:59:15 -0500

    add simple styling for profileClasses

--- client/src/components/profile/containers/ProfileClasses.js
+++ client/src/components/profile/containers/ProfileClasses.js
@@ -2,11 +2,29 @@ import React, { Component } from "react";
 
 function ProfileClasses(props) {
   return (
-    <div style={{ fontSize: "14px" }}>
-      <div>{props.classType} classes </div>
-      {props.classes.map((item, index) => (
-        <li> {item} </li>
-      ))}
+  	<div>
+	  	<div style={{
+	  		fontSize: "14px",
+	  		marginLeft: "50px"
+	  	}}>
+		  	<div style={{
+		  		textAlign: "left"
+		  	}}>
+		  	{props.classType} classes 
+		  	</div>
+	  	</div>
+  		<div style={{
+    		marginLeft: "60px"
+    		}}>
+		    <div style={{ 
+		    	fontSize: "14px",
+		    	textAlign: "left" 
+		    }}>
+		      {props.classes.map((item, index) => (
+		        <li> {item} </li>
+		      ))}
+		    </div>
+	    </div>
     </div>
   );
 }
