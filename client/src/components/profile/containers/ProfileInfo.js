e6443c6965f5866309cb49f618e56af01f375f7c
Stephen K <stephenjk@optonline.net>
2020-01-17 16:38:30 -0500

    pass props to ProfileInfo

--- client/src/components/profile/containers/ProfileInfo.js
+++ client/src/components/profile/containers/ProfileInfo.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
 export default class ProfileInfo extends Component {
-
+	constructor(props){
+		super(props);
+		this.state={
+			name: this.props.data.name,
+			lastname: this.props.data.lastname,
+			year: this.props.data.year,
+			major: this.props.data.major
+		}
+	}
 	render(){
 	console.log("pInfo");
 
 		return (
 			<div>
-				<div>Name: </div>
-				<div>Year: </div>
-				<div>Major: </div>
+				<div>Name: {this.state.name} </div>
+				<div>Lastname: {this.state.lastname} </div>
+				<div>Year: {this.state.year} </div>
+				<div>Major: {this.state.major} </div>
 			</div>
 		);
 	}
