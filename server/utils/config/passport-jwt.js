04d21136579eab8df29330b6e9690074fed83052
Marin <Marin.Marinov38@myhunter.cuny.edu>
2020-01-20 01:11:07 -0500

    strategy imported as function

--- server/utils/config/passport-jwt.js
+++ server/utils/config/passport-jwt.js
@@ -1,6 +1,5 @@
 import { Strategy, ExtractJwt } from "passport-jwt";
 import { User } from "../../db/models";
-import passport from "passport";
 import config from "./config";
 
 // show the strategy the token and how to decode it
@@ -9,16 +8,18 @@ const options = {
   secretOrKey: config.jwt_secret
 };
 
-passport.use(
-  new Strategy(options, async (payload, done) => {
-    try {
-      // find an existing user
-      const user = await User.findById(payload.id);
+export default function executeStrategy(passport) {
+  passport.use(
+    new Strategy(options, async (payload, done) => {
+      try {
+        // find an existing user
+        const user = await User.findById(payload.id);
 
-      if (!user) done(null, false);
-      else done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  })
-);
+        if (!user) done(null, false);
+        else done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    })
+  );
+}
