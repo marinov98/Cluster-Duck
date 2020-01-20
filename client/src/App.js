dd881c7eb6a47ef8ff0bd2a28e0d37170d3c3d6e
joshcwinton <joshcwinton@gmail.com>
2020-01-19 21:14:22 -0500

    Move Navbar from App.js into Navbar.js Also fixed warnings related to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom"
 import Login from "./components/auth/Login.js"
 import Register from "./components/auth/Register.js"
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from "reactstrap"
-// import logo from "./logo.svg";
+import DuckNavbar from "./components/Navbar.js"
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
     <Router>
     <Switch>
       <Route path="/login">
-        <Navbar light expand="md">
-          <NavbarBrand href="/">ClusterDuck 🦆</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav>
-              <NavItem>
-                <NavLink href="/profile">Profile</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/leaderboard">Leaderboard</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/chat">Chat</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <DuckNavbar />
         <Login />
       </Route>
       <Route path="/register">
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap"
+
+export default function DuckNavbar(){
+    return (
+      <Navbar light expand="md">
+        <NavbarBrand href="/">ClusterDuck<span role="img" aria-label="duck">🦆</span></NavbarBrand>
+        <Nav>
+          <NavItem>
+            <NavLink href="/profile">Profile</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/leaderboard">Leaderboard</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/chat">Chat</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    );
+};
