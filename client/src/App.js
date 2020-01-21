92ce980144af9128f35e5f386685a0192dfbe88f
joshcwinton <joshcwinton@gmail.com>
2020-01-20 22:21:38 -0500

    added temp user info, imported userprofile component

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login.js";
 import Register from "./components/auth/Register.js";
 import DuckNavbar from "./components/Navbar.js";
+import UserProfile from "./components/profile/UserProfile.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
 const backendPort = "localhost:3000";
+
 const axios = require('axios').default;
 
 const userId = "5e264d7302caf6abc735d32d";
@@ -12,9 +16,15 @@ const userId = "5e264d7302caf6abc735d32d";
 class App extends Component {
   constructor(props){
     super(props);
+    // TEMPORARY UNTIL GETUSERINFO WORKS
     this.state = {
-      userId: userId,
-      loggedIn: False
+      user: {
+        userId: "5e264d7302caf6abc735d32d",
+        username: "joshuaaaa",
+        email: "joshwinton@joshwinton.com",
+        firstName: "Josh",
+        lastName: "Winton",
+      }
     };
   }
 
@@ -41,6 +51,7 @@ class App extends Component {
             <Register />
           </Route>
           <Route path="/">
+            <UserProfile />
           </Route>
         </Switch>
       </Router>
