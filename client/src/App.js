1938805cb90ed4dceb3c741d83777d5d6cf4f6c7
Ajani Stewart <theamjaha@gmail.com>
2020-01-22 11:22:58 -0500

    ahh

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "./components/feed/feedPost"
+import FeedPost from "./components/feed/feedPost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <FeedPost />
     </div>
   );
 }
