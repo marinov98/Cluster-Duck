5e05933652918dd68459fbd5dcb56492c862a846
Ajani Stewart <theamjaha@gmail.com>
2020-01-27 17:49:41 -0500

    merge conflict

--- client/src/components/feed/feedPost.js
+++ client/src/components/feed/feedPost.js
@@ -1,10 +1,26 @@
 //layout of an individual post
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+// import axios from 'axios';
+import t from "./testfeeds";
 
+const b = t.body;
 
-function FeedPost() {
+export default function FeedPost() {
   /*
-    user info of the post 
+    call to api to get array of JSON with posts
   */
+  return (
+    <ListGroup>
+      {b.map(p => {
+        return (
+          <ListGroupItem>
+            <ListGroupItemHeading>{p.author + " says:"}</ListGroupItemHeading>
+            <ListGroupItemText>{p.body}</ListGroupItemText>
+          </ListGroupItem>
+        )
+      })}
+    </ListGroup>
+  );
 }
---
+++ client/src/components/feed/testfeeds.js
@@ -0,0 +1,14 @@
+const t = {
+  body: [
+    {
+      author: "Ajani",
+      body: "Fuck"
+    },
+    {
+      author: "Stewart",
+      body: "You"
+    }
+  ]
+};
+
+export default  t;
