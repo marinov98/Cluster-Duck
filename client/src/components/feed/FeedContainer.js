c35a78c32f84c4c28adbdb6645b3990504cb43cf
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-01-28 03:34:22 -0500

    connected with feed

--- client/src/components/feed/FeedContainer.js
+++ client/src/components/feed/FeedContainer.js
@@ -1,16 +1,14 @@
 //Layout of feed container!!!
 
-import React from 'react';
-import { Button, Jumbotron } from 'reactstrap';
-import FeedPost from './feedPost';
+import React from "react";
+import { Button, Jumbotron } from "reactstrap";
+import FeedPost from "./feedPost";
 // import axios from 'axios'
-import './FeedContainer.css';
+import "./FeedContainer.css";
 
 // const POST_URL = "https://cluster-duck-server.herokuapp.com/api/posts";
 
-
 export default function FeedContainer(props) {
-
   return (
     <div className="feedContainer">
       {/* { list of shits from user state} */}
@@ -18,12 +16,10 @@ export default function FeedContainer(props) {
         <h1>Welcome to your feed!</h1>
       </Jumbotron>
       <Button>New Post</Button>
-      <div className="posts">  
-        {
-          props.posts.map(p => {
-            return (<FeedPost post={p} />);
-          })
-        }
+      <div className="posts">
+        {props.posts.map((p, rank) => {
+          return <FeedPost post={p} key={rank + 1} />;
+        })}
       </div>
     </div>
   );
