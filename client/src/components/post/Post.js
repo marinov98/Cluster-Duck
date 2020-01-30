9a9496888d47194acc33fd962aec69c749508cd7
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-01-30 01:31:49 -0500

    beginning to implement add comment on the front end

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,64 +1,72 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Container, Jumbotron, Media, ListGroup, ListGroupItem } from 'reactstrap';
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  Container,
+  Jumbotron,
+  Media,
+  ListGroup,
+  ListGroupItem
+} from "reactstrap";
 
-class Post extends Component{
-    constructor(props){
-        super(props);
+class Post extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            postId: props.match.params.postid,
-            post: { upVotes: [], downVotes: []}
-        };
-    }
-
-    componentDidMount = async () => {
-        const postUrl = `https://cluster-duck-server.herokuapp.com/api/posts/${this.state.postId}`
-        try {
-            const { data } = await axios.get(postUrl);
-
-            if (data !== null) this.setState({post: data});
-            else this.props.history.push("/");
-        } catch (err) {
-            console.error(err);
-        }
+    this.state = {
+      postId: props.match.params.postid,
+      post: { upVotes: [], downVotes: [] },
+      replies: []
     };
-    
-    displayReplies = () => {
-        const replies = this.state.post.replies
-            .map((reply) => (
-                <ListGroupItem>{reply.text}</ListGroupItem>
-            ))
-        return(
-            <ListGroup>
-                {replies}
-            </ListGroup>
-        );
+  }
+
+  componentDidMount = async () => {
+    const postUrl = `https://cluster-duck-server.herokuapp.com/api/posts/${this.state.postId}`;
+    try {
+      const { data } = await axios.get(postUrl);
+
+      if (data !== null) this.setState({ post: data });
+      else this.props.history.push("/");
+
+      // fetch all replies
+      const replies = await axios.get(
+        `https://cluster-duck-server.herokuapp.com/api/replies/${this.state.postId}`
+      );
+      this.setState({ replies: replies.data });
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    render(){
-        console.log(this.state.post);
+  displayReplies = () => {
+    const replies = this.state.post.replies.map(reply => (
+      <ListGroupItem>{reply.text}</ListGroupItem>
+    ));
+    return <ListGroup>{replies}</ListGroup>;
+  };
 
-        const dateString = new Date(this.state.post.createdAt).toLocaleString();
-        const points = this.state.post.upVotes.length - this.state.post.downVotes.length;
+  render() {
+    console.log(this.state.post);
 
-        return (
-            <div style={{ textAlign: "center", paddingTop: "40px" }}>
-                <Container className="display-3">
-                    <Jumbotron>
-                        <h1>{this.state.post.title}</h1>
-                        <h5>{this.state.post.csTopic}</h5>
-                    </Jumbotron>
-                    <p style={{fontSize : "20px"}}>{dateString}</p>
-                    <p style={{fontSize : "20px"}}>{`${points} points`}</p>
-                    <Media middle body>
-                        <p style={{fontSize : "35px"}}>{this.state.post.text}</p>
-                    </Media>
-                </Container>
+    const dateString = new Date(this.state.post.createdAt).toLocaleString();
+    const points =
+      this.state.post.upVotes.length - this.state.post.downVotes.length;
 
-            </div>
-        );
-    }
+    return (
+      <div style={{ textAlign: "center", paddingTop: "40px" }}>
+        <Container className="display-3">
+          <Jumbotron>
+            <h1>{this.state.post.title}</h1>
+            <h5>{this.state.post.csTopic}</h5>
+          </Jumbotron>
+          <p style={{ fontSize: "20px" }}>{dateString}</p>
+          <p style={{ fontSize: "20px" }}>{`${points} points`}</p>
+          <Media middle body>
+            <p style={{ fontSize: "35px" }}>{this.state.post.text}</p>
+          </Media>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Post;
