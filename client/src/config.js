3f65dd7300d7c19ddd942f4ebad8ac09bb6d6e04
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-02-01 02:48:29 -0500

    looks like master config

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,6 @@
 import { config } from "dotenv";
 
-if (process.env.NODE_ENV !== "production") {
-  config();
-}
+if (process.env.NODE_ENV !== "production") config();
 
 export default {
   clientId: process.env.REACT_APP_CLIENT_ID
