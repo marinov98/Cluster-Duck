d0435b590f55e1a9a8feb26d0cce098460cdaa29
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-02-05 23:15:28 -0500

    this is just the graphql beginning

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,20 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./config";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export const client = new ApolloClient({
+  uri: "https://localhost:8080/graphql"
+});
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<ApolloApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
